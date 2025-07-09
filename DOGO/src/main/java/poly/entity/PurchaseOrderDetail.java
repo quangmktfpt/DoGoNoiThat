d042d22d06f159bc0d2e4076fede4263f3211890
nguyenducdung2006 <dungka2060@gmail.com>
2025-07-09 22:24:12 +0700

    DOGO

---
+++ DOGO/src/main/java/poly/entity/PurchaseOrderDetail.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.entity;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author quang
+ */
+public class PurchaseOrderDetail {
+     private Integer poDetailId;
+    private Integer purchaseOrderId;
+    private String productId;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+
+    public Integer getPoDetailId() {
+        return poDetailId;
+    }
+
+    public void setPoDetailId(Integer poDetailId) {
+        this.poDetailId = poDetailId;
+    }
+
+    public Integer getPurchaseOrderId() {
+        return purchaseOrderId;
+    }
+
+    public void setPurchaseOrderId(Integer purchaseOrderId) {
+        this.purchaseOrderId = purchaseOrderId;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public PurchaseOrderDetail() {
+    }
+
+    public PurchaseOrderDetail(Integer poDetailId, Integer purchaseOrderId, String productId, Integer quantity, BigDecimal unitPrice) {
+        this.poDetailId = poDetailId;
+        this.purchaseOrderId = purchaseOrderId;
+        this.productId = productId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+}
