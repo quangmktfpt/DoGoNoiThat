4a28704dfe11429ebf8606676a45541aa7a66270
Nghiatv2006 <trannghia2006nd@gmail.com>
2025-07-17 23:40:56 +0700

    Copy thư mục DOGO từ nhánh main

---
+++ DOGO/src/main/java/poly/entity/CartItem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poly.entity;
+
+/**
+ *
+ * @author quang
+ */
+public class CartItem {
+     private Integer cartItemId;
+    private Integer cartId;
+    private String productId;
+    private Integer quantity;
+}
