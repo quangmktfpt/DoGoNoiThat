b7ae7042587f5155a3df0c09f8d503a66a4591a2
Nghiatv2006 <trannghia2006nd@gmail.com>
2025-07-21 09:39:37 +0700

    Xoá sạch nội dung trong nhánh nghia

--- DOGO/src/main/java/poly/dao/CategoryDAO.java
+++
@@ -1,8 +0,0 @@
-package poly.dao;
-
-import poly.entity.Category;
-import java.util.List;
-
-public interface CategoryDAO extends CrudDAO<Category, String> {
-    // Có thể bổ sung các phương thức đặc thù cho Category nếu cần
-} 
